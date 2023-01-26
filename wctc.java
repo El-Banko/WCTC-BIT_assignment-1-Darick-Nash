0be034128970739d5a0159baea953ca050b7dbf2
El-Banko <114441059+El-Banko@users.noreply.github.com>
2023-01-26 17:20:57 -0600

    Add files via upload

---
+++ wctc.java
@@ -0,0 +1,53 @@
+package edu;
+
+import java.util.Scanner;
+
+public class wctc {
+
+    public static void main(String[] args) {
+        Scanner keyborad = new Scanner(System.in);
+        // Create a Scanner object
+        System.out.println("Enter 1 or 2");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("Enter 1 or 2: " + userName);  // Output user input
+        String number = myObj.nextLine();
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("mild or spicy");
+        String number = myObj.nextLine();
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("tea or coffee");
+        String number = myObj.nextLine();
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("breakfast or brunch");
+        String number = myObj.nextLine();
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("summer or winter");
+        char c = 'paper or plastic';
+        String c = myObj.nextLine();
+        System.out.print("Enter a number: ");
+        int number = Interger.parseInt(keyboard.nextLine());
+        System.out.println("You enterd:" + number);
+        System.out.println("paper or plastic");
+        if (number > 3
+        System.out.println("Large Number");
+         else if (number < 3)
+            System.out.println("Small Number");
+        else
+            System.out.println("Exatcly 3");
+
+
+    }
+}
+
